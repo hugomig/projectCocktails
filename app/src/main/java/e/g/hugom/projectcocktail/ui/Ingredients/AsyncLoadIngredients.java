2c96569fbd60c70c5fd88b802b2a56d6b5536a84
Juan-Miguelitos <73351499+Juan-Miguelitos@users.noreply.github.com>
2021-03-16 11:06:47 +0100

    Ingredients

---
+++ app/src/main/java/e/g/hugom/projectcocktail/ui/Ingredients/AsyncLoadIngredients.java
@@ -0,0 +1,58 @@
+package e.g.hugom.projectcocktail.ui.Ingredients;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.IOException;
+
+public class AsyncLoadIngredients extends AsyncTask<String,Void,JSONObject> {
+
+    @Override
+    protected JSONObject doInBackground(String... strings){
+        URL url = null;
+        try{
+            url = new URL(strings[0]);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try{
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                String response = readStream(in);
+                JSONObject result = new JSONObject(response);
+            } catch (JSONException e){
+                e.printStackTrace();
+            } finally {urlConnection.disconnect();}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject){
+        Log.i("antoine",jsonObject.toString());
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+
+
+}
--- app/src/main/java/e/g/hugom/projectcocktail/ui/Ingredients/IngredientsFragment.java
+++ app/src/main/java/e/g/hugom/projectcocktail/ui/Ingredients/IngredientsFragment.java
@@ -18,6 +18,9 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
         return root;
     }
 }
