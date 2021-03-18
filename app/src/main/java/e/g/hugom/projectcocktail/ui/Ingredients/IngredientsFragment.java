139c5d16a9d6b990a8c2e912c333aff8b1b35fbf
Juan-Miguelitos <73351499+Juan-Miguelitos@users.noreply.github.com>
2021-03-18 22:08:29 +0100

    ImageRequest_Ingredient

--- app/src/main/java/e/g/hugom/projectcocktail/ui/Ingredients/IngredientsFragment.java
+++ app/src/main/java/e/g/hugom/projectcocktail/ui/Ingredients/IngredientsFragment.java
@@ -1,18 +1,27 @@
 package e.g.hugom.projectcocktail.ui.Ingredients;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+
 import java.util.ArrayList;
 
+import e.g.hugom.projectcocktail.MySingleton;
 import e.g.hugom.projectcocktail.R;
 
 public class IngredientsFragment extends Fragment {
@@ -21,10 +30,11 @@ public class IngredientsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_ingredients, container, false);
-        AdapterIngredients adapter = new AdapterIngredients(getLayoutInflater());
+        ListView ingredientList = root.findViewById(R.id.ingredient_list);
+        RequestQueue queue = MySingleton.getInstance(ingredientList.getContext()).getRequestQueue();
+        AdapterIngredients adapter = new AdapterIngredients(getLayoutInflater(), queue);
         AsyncLoadIngredients asyncLoadIngredients = new AsyncLoadIngredients(adapter);
         asyncLoadIngredients.execute(urlIngredients);
-        ListView ingredientList = root.findViewById(R.id.ingredient_list);
         ingredientList.setAdapter(adapter);
 
 
@@ -36,10 +46,12 @@ class AdapterIngredients extends BaseAdapter {
 
     private ArrayList<String> ingredientsNames;
     private LayoutInflater inflater;
+    private RequestQueue queue;
 
-    public AdapterIngredients(LayoutInflater inflater){
+    public AdapterIngredients(LayoutInflater inflater, RequestQueue queue){
         this.inflater = inflater;
         ingredientsNames = new ArrayList<>();
+        this.queue = queue;
     }
 
 
@@ -63,8 +75,20 @@ class AdapterIngredients extends BaseAdapter {
         convertView = inflater.inflate(R.layout.ingredient_row_layout, parent, false);
         TextView ingredientName = convertView.findViewById(R.id.ingredient_name);
         ingredientName.setText(ingredientsNames.get(position));
-
-
+        ImageView ingredientPicture = convertView.findViewById(R.id.ingredient_picture);
+        ImageRequest imageRequest = new ImageRequest("https://www.thecocktaildb.com/images/ingredients/" + ingredientsNames.get(position) + ".png", new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                ingredientPicture.setImageBitmap(response);
+            }
+        }, 0, 0, ImageView.ScaleType.CENTER, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("antoine", error.toString());
+            }
+        });
+        queue.add(imageRequest);
+        
         return convertView;
     }
 
